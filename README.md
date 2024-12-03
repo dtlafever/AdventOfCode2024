9484a3b9d444b262932b0f15e7c657a2b8413589
Derek LaFever <dtlafever@gmail.com>
2024-12-03 10:08:41 +0100

    Notes and Day 3
    - added my notes to each of the days I have worked on in the readme
    - completed day 3

--- README.md
+++ README.md
@@ -1,7 +1,30 @@
 # AdventOfCode2024
  
 ## Development Environment
-`uv venv --python 3.12`
-`.venv\Scripts\activate`
+```
+uv venv --python 3.12
+.venv\Scripts\activate
+```
 
 ## Day 1 Notes
+I felt pretty confident with my solution for both parts of the problem. In part one you simply need to 
+parse the input into two lists and sort them. Then you can iterate through the lists and find the
+distance. The second part was similar, but instead you create a dictionary with the occurrences of each
+number in the right list. The small trick is to use `defaultdict` so you default to 0 if the key is not
+found.
+
+## Day 2 Notes
+I must admit, the second part of this problem took me way longer than I like to admit. I must have tried
+for an hour and a half trying to account for all the tiny details and edge cases. 
+I finally realized the simple solution was the best one. Simply try omitting 1 level at a time from
+a report until you either find that it is safe (using my solution from part 1). If you do not
+find a solution, then the report is not safe. This does have a big O of N*N since in the worst case
+you have to omit all the levels, but it is a simple solution that works and it MUCH less complicated
+to implement and understand. Sometimes code that is readable is worth it.
+
+## Day 3 Notes
+Regular Expressions to the rescue! The first part was very easy with the proper regular expression.
+The second part was a bit more tricky, but I figured it out in the end by keeping track of 
+the indices in the string where `do()` and `don't()` were found. Once I have the indices,
+I can then cut up the string into "enabled" strings where I can calculate the sum of the products.
+I don't love the solution as it requires a few for loops, but it works.
---
+++ src/day3.py
@@ -0,0 +1,94 @@
+# Problem: https://adventofcode.com/2024/day/3#part3
+
+import re
+
+# (?<=mul\()  => lookbehind positive for "mul(", but does not capture it
+# ([0-9]{1,3},[0-9]{1,3}) => captures 1-3 digits, followed by a comma, followed by 1-3 digits
+# (?=\)) => lookahead positive for ")", but does not capture it
+# EX. mul(1,564)
+MULT_RE_EXPRESSION = r"(?<=mul\()([0-9]{1,3},[0-9]{1,3})(?=\))"
+
+# EX. do()
+DO_RE_EXPRESSION = r"(do\(\))"
+DO_STR = "do()"
+# EX. don't()
+DONT_RE_EXPRESSION = r"(don't\(\))"
+DONT_STR = "don't()"
+
+DO_AND_DONT_RE_EXPRESSION = r"(do\(\)|don't\(\))"
+
+def puzzle1():
+    total_sum_of_products = 0
+
+    with open('../data/input3.txt') as f:
+        for line in f:
+            # find all matches for the MULT_RE_EXPRESSION
+            # EX. mul(1,564)
+            matches = re.findall(MULT_RE_EXPRESSION, line)
+
+            for match in matches:
+                num1, num2 = match.split(',')
+                product = int(num1) * int(num2)
+                total_sum_of_products += product
+
+                # print(matches)
+
+    print(total_sum_of_products)
+
+def puzzle2():
+    lines = [] # for combining file lines to one string
+    with open('../data/input3.txt') as f:
+        for line in f:
+            lines.append(line)
+
+    is_currently_enabled = True
+    combined_lines = ''.join(lines)
+    enabled_index_start = 0
+    enabled_index_end = -1
+    enabled_indices = []
+    # loop through all instances of "do()" and "don't()" and track
+    # the indices of the enabled sections
+    for match in re.finditer(DO_AND_DONT_RE_EXPRESSION, combined_lines):
+
+        if match.group() == DO_STR:
+            if not is_currently_enabled:
+                # start tracking the enabled index
+                is_currently_enabled = True
+                _, enabled_index_start = match.span()
+
+                enabled_index_end = -1
+        elif match.group() == DONT_STR:
+            if is_currently_enabled:
+                # stop tracking the enabled index
+                is_currently_enabled = False
+                enabled_index_end, _ = match.span()
+                enabled_indices.append((enabled_index_start, enabled_index_end))
+
+                enabled_index_start = -1
+
+    # Special Case if there is no "don't()" at the end of the file
+    if is_currently_enabled:
+        enabled_indices.append((enabled_index_start, -1))
+
+    # calculate the sum of the products for the enabled indices
+    total_sum_of_products = 0
+    for start, end in enabled_indices:
+        enabled_line = combined_lines[start:end]
+        # find all matches for the MULT_RE_EXPRESSION
+        # EX. mul(1,564)
+        matches = re.findall(MULT_RE_EXPRESSION, enabled_line)
+
+        for match in matches:
+            num1, num2 = match.split(',')
+            product = int(num1) * int(num2)
+            total_sum_of_products += product
+
+
+    print(total_sum_of_products)
+
+def main():
+    puzzle1()
+    puzzle2()
+
+if __name__ == '__main__':
+    main()
